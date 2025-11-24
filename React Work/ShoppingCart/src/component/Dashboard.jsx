834457f8c9a04bec0a3a96eaacc5152fdcbf3df3
rajnandini0409 <rajnandinisoni04@gmail.com>
2025-11-24 13:07:52 +0530

    till addToCart

--- React Work/ShoppingCart/src/component/Dashboard.jsx
+++ React Work/ShoppingCart/src/component/Dashboard.jsx
@@ -1,20 +1,44 @@
 import React, { useEffect,useState } from 'react'
 
 function Dashboard() {
-  const[counter,setCounter]=useState(10);
-  const[pointer,setPointer]=useState(50);
+const [data,setData]=useState([]);
   useEffect(()=>{
-    console.log(counter);
-    console.log(pointer);
-  },[counter])
+    async function getData(){
+      const response=await fetch ("https://fakestoreapi.com/products");
+      const resData=await response.json();
+      setData(resData);
+      console.log(resData);
+    }
+    getData();
+  },[])
+  
+function addToCart(ele){
+ alert(ele.title);
+}
+
+
   return (
-    <div>
-      <div>
-        <h2> Counter value={counter}</h2>
-        <h2> Pointer value={pointer}</h2>
-      </div>
-      <button onClick={()=>setCounter(counter+20)}>Counter</button>
-      <button onClick={()=>setPointer(pointer+1)}>Pointer</button>
+    <div> 
+     {/* {JSON.stringify(data)} */}
+     {
+      data.length===0?(
+        <h2>Data not found</h2>
+      ):(
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '50px', margin:'100px' }}>
+         {
+          data.map((ele)=>(
+            <div style={{border:'2px solid red', height:'700px', width:'300px'}}>
+              <img src={ele.image} height={100} width={100}/>
+              <h3>{ele.title}</h3>
+              <p>{ele.description}</p>
+              <h4>{ele.price}</h4>
+              <button onClick={()=>addToCart(ele)}>Add to Cart</button>
+            </div>
+          ))
+         }
+        </div>
+      )
+     }
     </div>
   )
 }
